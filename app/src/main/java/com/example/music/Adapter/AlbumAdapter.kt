0c33639fb0e0b535804fdc6c10d8889337d59779
MinZue14 <tramtnn.22ds@vku.udn.vn>
2024-06-14 15:18:22 +0700

    cham commit

--- app/src/main/java/com/example/music/Adapter/AlbumAdapter.kt
+++ app/src/main/java/com/example/music/Adapter/AlbumAdapter.kt
@@ -1,32 +1,201 @@
-//package com.example.music.Adapter
-//
-//import android.app.Activity
-//import android.view.ViewGroup
-//import androidx.recyclerview.widget.RecyclerView
-//import com.example.music.Data
-//
-//class AlbumAdapter (var context: Activity, var dataList: List<Data>) :
-//    RecyclerView.Adapter<AlbumAdapter.AlbumViewHolder>(){
-//    // Khai báo một interface để xử lý sự kiện click
-//    interface OnAlbumClickListener {
-//        fun onItemClick(data: Data)
-//    }
-//
-//    // Biến để lưu trữ listener của sự kiện click
-//    var onItemClickListener: OnAlbumClickListener? = null
-//
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlbumViewHolder {
-//        TODO("Not yet implemented")
-//    }
-//
-//    override fun getItemCount(): Int {
-//        TODO("Not yet implemented")
-//    }
-//
-//    override fun onBindViewHolder(holder: AlbumViewHolder, position: Int) {
-//        TODO("Not yet implemented")
-//    }
-//    class AlbumViewHolder {
-//
-//    }
-//}
+package com.example.music.Adapter
+
+import android.app.Activity
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.example.music.Data
+import com.example.music.Database.DatabaseUserList
+import com.example.music.Database.DatabaseUserPlaylist
+import com.example.music.R
+import com.squareup.picasso.Picasso
+
+class AlbumAdapter (var context: Activity, var dataList: List<Data>) :
+    RecyclerView.Adapter<AlbumAdapter.AlbumViewHolder>(){
+
+    // Khai báo một interface để xử lý sự kiện click
+    interface OnAlbumClickListener {
+        fun onItemClick(data: Data)
+    }
+
+    // Biến để lưu trữ listener của sự kiện click
+    var onItemClickListener: OnAlbumClickListener? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlbumViewHolder {
+        val itemView = LayoutInflater.from(context).inflate(R.layout.layout_list_track, parent, false)
+        return AlbumViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return dataList.size
+    }
+
+    override fun onBindViewHolder(holder: AlbumViewHolder, position: Int) {
+        val currentData = dataList[position]
+
+        holder.trackName.text = currentData.title
+        holder.trackArtist.text = currentData.artist.name
+
+        Picasso.get().load(currentData.album.cover).into(holder.trackImage);
+
+//giao diện nhạc
+        // Thiết lập sự kiện click cho mỗi mục trong danh sách
+        holder.itemView.setOnClickListener {
+            // Kiểm tra xem listener đã được thiết lập hay chưa
+            onItemClickListener?.onItemClick(currentData)
+        }
+//favorite
+        // Kiểm tra xem bài hát đã tồn tại trong danh sách yêu thích của người dùng hay không
+        val dbManager = DatabaseUserList(context)
+        val sharedPref = context.getSharedPreferences("user_data", Context.MODE_PRIVATE)
+        val userIDString = sharedPref.getString("userID", null)
+        val userID = userIDString?.toLongOrNull() ?: -1L // Chuyển đổi thành Long hoặc gán mặc định là -1L nếu không thành công
+
+        if (userID != -1L) {
+            val trackID = currentData.id
+            val isTrackInFavorites = dbManager.isTrackInFavorites(userID, trackID)
+
+            if (isTrackInFavorites) {
+                // Bài hát đã được yêu thích, hiển thị hình ảnh yêu thích đã chọn
+                holder.imageButtonLove.setImageResource(R.drawable.baseline_favorite_red)
+            } else {
+                // Bài hát chưa được yêu thích, hiển thị hình ảnh yêu thích bình thường
+                holder.imageButtonLove.setImageResource(R.drawable.baseline_favorite)
+            }
+        }
+
+        // Thiết lập sự kiện click cho buttonLove
+        holder.imageButtonLove.setOnClickListener {
+
+            if (userID != -1L) {
+                val currentData = dataList[position]
+                val trackID = currentData.id
+
+                // Kiểm tra xem bài hát đã tồn tại trong danh sách yêu thích của người dùng hay không
+                val isTrackFavorited = isTrackInFavorites(userID, trackID)
+
+                if (!isTrackFavorited) {
+                    // Thêm bài hát vào danh sách yêu thích của người dùng
+                    val dbManager = DatabaseUserList(context)
+                    val isSuccess = dbManager.addTrackToFavorite(userID, trackID)
+                    if (isSuccess) {
+                        Toast.makeText(context, "Bạn đã yêu thích bài hát này", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(context, "Xỏa bài hát khỏi danh sách yêu thích", Toast.LENGTH_SHORT).show()
+                    }
+
+                    // Thay đổi hình ảnh của buttonLove thành btnLoveRed
+                    holder.imageButtonLove.setImageResource(R.drawable.baseline_favorite_red)
+
+                } else {
+                    // Xóa bài hát khỏi danh sách yêu thích của người dùng
+                    val dbManager = DatabaseUserList(context)
+                    val isSuccess = dbManager.removeTrackFromFavorite(userID, trackID)
+                    if (isSuccess) {
+                        Toast.makeText(context, "Bạn hong yêu thích bài này", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(context, "Xỏa bài hát khỏi danh sách yêu thích", Toast.LENGTH_SHORT).show()
+                    }
+
+                    // Đặt lại hình ảnh ban đầu của buttonLove
+                    holder.imageButtonLove.setImageResource(R.drawable.baseline_favorite)
+                }
+            } else {
+                Toast.makeText(context, "Không thể xác định người dùng", Toast.LENGTH_SHORT).show()
+            }
+        }
+//playlist
+        // Kiểm tra xem bài hát đã tồn tại trong danh sách playlist của người dùng hay không
+        val dbManager1 = DatabaseUserPlaylist(context)
+
+        if (userID != -1L) {
+            val trackID = currentData.id
+            val isTrackInPlaylist = dbManager1.isTrackInPlaylist(userID, trackID)
+
+            if (isTrackInPlaylist) {
+                // Bài hát đã được thêm vào playlist, hiển thị hình ảnh playlist đã chọn
+                holder.imageButtonPlaylist.setImageResource(R.drawable.baseline_playlist_add_check)
+            } else {
+                // Bài hát chưa được thm vào playlist, hiển thị hình ảnh playlist bình thường
+                holder.imageButtonPlaylist.setImageResource(R.drawable.baseline_genre)
+            }
+        }
+
+        // Thiết lập sự kiện click cho buttonPlaylist
+        holder.imageButtonPlaylist.setOnClickListener {
+
+            if (userID != -1L) {
+                val currentData = dataList[position]
+                val trackID = currentData.id
+
+                // Kiểm tra xem bài hát đã tồn tại trong Playlist của người dùng hay không
+                val isTrackPlaylist = isTrackInPlaylist(userID, trackID)
+
+                if (!isTrackPlaylist) {
+                    // Thêm bài hát vào Playlist của người dùng
+                    val dbManager1 = DatabaseUserPlaylist(context)
+                    val isSuccess = dbManager1.addTrackToPlaylist(userID, trackID)
+                    if (isSuccess) {
+                        Toast.makeText(context, "Bạn đã thêm bài hát này vào playlist", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(context, "Xóa bài hát khỏi playlist", Toast.LENGTH_SHORT).show()
+                    }
+
+                    // Thay đổi hình ảnh của imageButtonPlaylist thành baseline_playlist_add_check
+                    holder.imageButtonPlaylist.setImageResource(R.drawable.baseline_playlist_add_check)
+
+                } else {
+                    // Xóa bài hát khỏi playlist của người dùng
+                    val dbManager1 = DatabaseUserPlaylist(context)
+                    val isSuccess = dbManager1.removeTrackFromPlaylist(userID, trackID)
+                    if (isSuccess) {
+                        Toast.makeText(context, "Bạn hong bỏ bài hát này vào playlist ", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(context, "Xóa bài hát khỏi playlist", Toast.LENGTH_SHORT).show()
+                    }
+
+                    // Đặt lại hình ảnh ban đầu của buttonLove
+                    holder.imageButtonPlaylist.setImageResource(R.drawable.baseline_genre)
+                }
+            } else {
+                Toast.makeText(context, "Không thể xác định người dùng", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+    private fun isTrackInFavorites(userID: Long, trackID: Long): Boolean {
+        val dbManager = DatabaseUserList(context)
+        return dbManager.isTrackInFavorites(userID, trackID)
+    }
+    private fun isTrackInPlaylist(userID: Long, trackID: Long): Boolean {
+        val dbManager1 = DatabaseUserPlaylist(context)
+        return dbManager1.isTrackInPlaylist(userID, trackID)
+    }
+
+    class AlbumViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val trackImage: ImageView
+
+        val trackName: TextView
+        val trackArtist: TextView
+        val trackView: TextView
+
+        val imageButtonPlaylist: ImageButton
+        val imageButtonLove: ImageButton
+
+        init {
+            trackImage = itemView.findViewById(R.id.trackImage)
+            trackName = itemView.findViewById(R.id.trackName)
+            trackArtist = itemView.findViewById(R.id.trackArtist)
+            trackView = itemView.findViewById(R.id.trackView)
+            imageButtonPlaylist = itemView.findViewById(R.id.imageButtonPlaylist)
+            imageButtonLove = itemView.findViewById(R.id.imageButtonLove)
+        }
+    }
+}
--- app/src/main/java/com/example/music/Main/AlbumActivity.kt
+++ app/src/main/java/com/example/music/Main/AlbumActivity.kt
@@ -1,179 +1,179 @@
-//package com.example.music.Main
-//
-//import android.content.Context
-//import android.content.Intent
-//import android.content.SharedPreferences
-//import android.os.Bundle
-//import android.util.Log
-//import androidx.appcompat.app.AppCompatActivity
-//import androidx.core.view.GravityCompat
-//import androidx.drawerlayout.widget.DrawerLayout
-//import androidx.recyclerview.widget.LinearLayoutManager
-//import androidx.recyclerview.widget.RecyclerView
-//import com.example.music.Adapter.AlbumAdapter
-//import com.example.music.Adapter.ArtistAdapter
-//import com.example.music.Admin.AdminLogin
-//import com.example.music.Album
-//import com.example.music.ApiInterface
-//import com.example.music.Artist
-//import com.example.music.Data
-//import com.example.music.R
-//import com.example.music.databinding.ActivityAlbumBinding
-//import com.example.music.databinding.HeaderMenuBinding
-//import com.google.android.material.navigation.NavigationView
-//import com.squareup.picasso.Picasso
-//import retrofit2.Call
-//import retrofit2.Callback
-//import retrofit2.Response
-//import retrofit2.Retrofit
-//import retrofit2.converter.gson.GsonConverterFactory
-//
-//class AlbumActivity : AppCompatActivity() {
-//    lateinit var binding: ActivityAlbumBinding
-//    lateinit var sharedPref: SharedPreferences
-//    private lateinit var dataList: List<Data>
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        binding = ActivityAlbumBinding.inflate(layoutInflater)
-//        val view = binding.root
-//        setContentView(view)
-//
-////menu
-//        // Khai báo drawerLayout và navigationView
-//        val drawer = findViewById<DrawerLayout>(R.id.users)
-//        val navigationView = findViewById<NavigationView>(R.id.nav_view)
-//
-//        // Khởi tạo SharedPreferences
-//        sharedPref = getSharedPreferences("user_data", Context.MODE_PRIVATE)
-//
-//        // Lấy thông tin người dùng từ SharedPreferences
-//        val username = sharedPref.getString("username", "") ?: ""
-//        val email = sharedPref.getString("email", "") ?: ""
-//
-//        // Gán headerLayoutBinding cho navigationView menu
-//        val headerLayoutBinding = HeaderMenuBinding.bind(navigationView.getHeaderView(0))
-//        headerLayoutBinding.menuUsername.text = username
-//        headerLayoutBinding.menuUsermail.text = email
-//
-//        // Thiết lập listener cho các mục menu trong navigationView
-//        navigationView.setNavigationItemSelectedListener { menuItem ->
-//            when (menuItem.itemId) {
-//                R.id.navHome -> {
-//                    val intent = Intent(this, MainActivity::class.java)
-//                    startActivity(intent)
-//                    true
-//                }
-//
-//                R.id.navListLiked -> {
-//                    val intent = Intent(this, UserFavoriteListActivity::class.java)
-//                    startActivity(intent)
-//                    true
-//                }
-//
-//                R.id.navPlaylist -> {
-//                    val intent = Intent(this, UserPlaylistActivity::class.java)
-//                    startActivity(intent)
-//                    true
-//                }
-//
-//                R.id.navChart -> {
-//                    val intent = Intent(this, ChartActivity::class.java)
-//                    startActivity(intent)
-//                    true
-//                }
-//
-//                R.id.nav_user_to_admin -> {
-//                    val intent = Intent(this, AdminLogin::class.java)
-//                    startActivity(intent)
-//                    true
-//                }
-//
-//                R.id.navSetting -> {
-//                    val intent = Intent(this, MainActivity::class.java)
-//                    startActivity(intent)
-//                    true
-//                }
-//
-//                R.id.navLogOut -> {
-//                    val intent = Intent(this, LoginActivity::class.java)
-//                    startActivity(intent)
-//                    true
-//                }
-//
-//
-//                else -> false
-//            }
-//        }
-//
-//        // Xử lý sự kiện khi người dùng nhấn nút mở Drawer
-//        binding.btnOpenDrawer.setOnClickListener {
-//            drawer.openDrawer(GravityCompat.START)
-//        }
-//
-////main
-//// Khởi tạo Retrofit
-//        val retrofitBuilder = Retrofit.Builder()
-//            .baseUrl("https://deezerdevs-deezer.p.rapidapi.com/")
-//            .addConverterFactory(GsonConverterFactory.create())
-//            .build()
-//            .create(ApiInterface::class.java)
-//
-//        val albumId = intent.getStringExtra("albumId")
-//        Log.d("TAG", "Album ID from intent: $albumId") // Debug albumID
-//
-//        if (!albumId.isNullOrEmpty()) {
-//            val albumIdLong = albumId.toLong()
-//            val retrofitData = retrofitBuilder.getAlbumDetail(albumIdLong)
-//            Log.d("TAG", "Album id form intent: ${albumIdLong}")
-//
-//            retrofitData.enqueue(object : Callback<Album?> {
-//                override fun onResponse(call: Call<Album?>, response: Response<Album?>) {
-//                    if (response.isSuccessful) {
-//                        val album = response.body()
-//                        if (album != null) {
-//
-//                            // Hiển thị giao diện album từ dữ liệu API
-//                            binding.albumName.text = album.title
-//                            Picasso.get().load(album.cover_medium).into(binding.albumImage)
-//
-//                            // Khởi tạo RecyclerView và Adapter
-//                            val trackList = findViewById<RecyclerView>(R.id.trackList)
-//                            val adapter = AlbumAdapter(this@AlbumActivity, dataList)
-//
-//                            // Thiết lập LayoutManager cho RecyclerView
-//                            trackList.layoutManager = LinearLayoutManager(this@AlbumActivity, LinearLayoutManager.HORIZONTAL, false)
-//
-//                            // Thiết lập Adapter cho RecyclerView
-//                            trackList.adapter = adapter
-//
-//                            // Thiết lập listener cho adapter
-//                            adapter.onItemClickListener = object : ArtistAdapter.OnArtistClickListener{
-//                                override fun onItemClick(data: Data) {
-//                                    // Mở giao diện nhạc của bài hát được nhấp
-//                                    val intent = Intent(this@AlbumActivity, MusicActivity::class.java)
-//                                    intent.putExtra("trackId", data.id.toString()
-//                                    ) // Truyền ID của bài hát qua intent
-//                                    startActivity(intent)
-//                                }
-//                            }
-//
-//                            Log.d("TAG", "album Name: ${album.title}")
-//                        } else {
-//                            Log.d("TAG", "No album data received")
-//                        }
-//                    } else {
-//                        Log.d("TAG", "Response not successful: ${response.code()}")
-//                        Log.d("TAG", "Error body: ${response.errorBody()?.string()}")
-//                    }
-//                }
-//
-//                override fun onFailure(call: Call<Album?>, t: Throwable) {
-//                    Log.d("TAG", "onFailure: ${t.message}")
-//                }
-//            })
-//        } else {
-//            Log.d("TAG", "album ID is null or empty")
-//        }
-//    }
-//}
+package com.example.music.Main
+
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.music.Adapter.AlbumAdapter
+import com.example.music.Adapter.ArtistAdapter
+import com.example.music.Admin.AdminLogin
+import com.example.music.Album
+import com.example.music.ApiInterface
+import com.example.music.Artist
+import com.example.music.Data
+import com.example.music.R
+import com.example.music.databinding.ActivityAlbumBinding
+import com.example.music.databinding.HeaderMenuBinding
+import com.google.android.material.navigation.NavigationView
+import com.squareup.picasso.Picasso
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class AlbumActivity : AppCompatActivity() {
+    lateinit var binding: ActivityAlbumBinding
+    lateinit var sharedPref: SharedPreferences
+    private lateinit var dataList: List<Data>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAlbumBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+//menu
+        // Khai báo drawerLayout và navigationView
+        val drawer = findViewById<DrawerLayout>(R.id.users)
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+
+        // Khởi tạo SharedPreferences
+        sharedPref = getSharedPreferences("user_data", Context.MODE_PRIVATE)
+
+        // Lấy thông tin người dùng từ SharedPreferences
+        val username = sharedPref.getString("username", "") ?: ""
+        val email = sharedPref.getString("email", "") ?: ""
+
+        // Gán headerLayoutBinding cho navigationView menu
+        val headerLayoutBinding = HeaderMenuBinding.bind(navigationView.getHeaderView(0))
+        headerLayoutBinding.menuUsername.text = username
+        headerLayoutBinding.menuUsermail.text = email
+
+        // Thiết lập listener cho các mục menu trong navigationView
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.navHome -> {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+
+                R.id.navListLiked -> {
+                    val intent = Intent(this, UserFavoriteListActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+
+                R.id.navPlaylist -> {
+                    val intent = Intent(this, UserPlaylistActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+
+                R.id.navChart -> {
+                    val intent = Intent(this, ChartActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+
+                R.id.nav_user_to_admin -> {
+                    val intent = Intent(this, AdminLogin::class.java)
+                    startActivity(intent)
+                    true
+                }
+
+                R.id.navSetting -> {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+
+                R.id.navLogOut -> {
+                    val intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+
+
+                else -> false
+            }
+        }
+
+        // Xử lý sự kiện khi người dùng nhấn nút mở Drawer
+        binding.btnOpenDrawer.setOnClickListener {
+            drawer.openDrawer(GravityCompat.START)
+        }
+
+//main
+// Khởi tạo Retrofit
+        val retrofitBuilder = Retrofit.Builder()
+            .baseUrl("https://deezerdevs-deezer.p.rapidapi.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiInterface::class.java)
+
+        val albumId = intent.getStringExtra("albumId")
+        Log.d("TAG", "Album ID from intent: $albumId") // Debug albumID
+
+        if (!albumId.isNullOrEmpty()) {
+            val albumIdLong = albumId.toLong()
+            val retrofitData = retrofitBuilder.getAlbumDetail(albumIdLong)
+            Log.d("TAG", "Album id form intent: ${albumIdLong}")
+
+            retrofitData.enqueue(object : Callback<Album?> {
+                override fun onResponse(call: Call<Album?>, response: Response<Album?>) {
+                    if (response.isSuccessful) {
+                        val album = response.body()
+                        if (album != null) {
+
+                            // Hiển thị giao diện album từ dữ liệu API
+                            binding.albumName.text = album.title
+                            Picasso.get().load(album.cover_medium).into(binding.albumImage)
+
+                            // Khởi tạo RecyclerView và Adapter
+                            val trackList = findViewById<RecyclerView>(R.id.trackList)
+                            val adapter = AlbumAdapter(this@AlbumActivity, dataList)
+
+                            // Thiết lập LayoutManager cho RecyclerView
+                            trackList.layoutManager = LinearLayoutManager(this@AlbumActivity, LinearLayoutManager.HORIZONTAL, false)
+
+                            // Thiết lập Adapter cho RecyclerView
+                            trackList.adapter = adapter
+
+                            // Thiết lập listener cho adapter
+                            adapter.onItemClickListener = object : AlbumAdapter.OnAlbumClickListener{
+                                override fun onItemClick(data: Data) {
+                                    // Mở giao diện nhạc của bài hát được nhấp
+                                    val intent = Intent(this@AlbumActivity, MusicActivity::class.java)
+                                    intent.putExtra("trackId", data.id.toString()
+                                    ) // Truyền ID của bài hát qua intent
+                                    startActivity(intent)
+                                }
+                            }
+
+                            Log.d("TAG", "album Name: ${album.title}")
+                        } else {
+                            Log.d("TAG", "No album data received")
+                        }
+                    } else {
+                        Log.d("TAG", "Response not successful: ${response.code()}")
+                        Log.d("TAG", "Error body: ${response.errorBody()?.string()}")
+                    }
+                }
+
+                override fun onFailure(call: Call<Album?>, t: Throwable) {
+                    Log.d("TAG", "onFailure: ${t.message}")
+                }
+            })
+        } else {
+            Log.d("TAG", "album ID is null or empty")
+        }
+    }
+}
